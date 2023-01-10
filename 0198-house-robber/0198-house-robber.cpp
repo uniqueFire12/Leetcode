eb978a663aceea7f693cea4614e12e23fca60bf9
Ananya Ganguly <107770579+uniqueFire12@users.noreply.github.com>
2023-01-10 14:31:59 +0530

    Time: 5 ms (24.74%), Space: 7.6 MB (96.06%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+      int n = nums.size();
+      int prev = nums[0], prev2 = 0;
+      
+      for(int i=1; i<n; i++) {
+        int take = nums[i];
+        if(i > 1) take += prev2;
+        int nottake = 0 + prev;
+        
+        int curi = max(take, nottake);
+        prev2 = prev;
+        prev = curi;
+      }
+      return prev;
+    }
+};
