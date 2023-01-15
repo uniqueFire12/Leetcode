8b820afc1e19f88b02531f15e0ea45cdc2a0c36e
Ananya Ganguly <107770579+uniqueFire12@users.noreply.github.com>
2023-01-15 22:01:16 +0530

    Time: 11 ms (75.14%), Space: 10 MB (66.67%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -13,25 +13,28 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
       int n = grid.size();
       int m = grid[0].size();
-      vector<vector<int>> dp(n, vector<int>(m, 0));
+      //vector<vector<int>> dp(n, vector<int>(m, 0));
       // return f(n-1, m-1, grid, dp);
       
-      // tabulation approach
+      // space optimization approach
+      vector<int> prev(m, 0);
       for(int i=0; i<n; i++) {
+        vector<int> curr(m, 0);
         for(int j=0; j<m; j++) {
-          if(i == 0 && j == 0) dp[i][j] = grid[0][0];
+          if(i == 0 && j == 0) curr[j] = grid[0][0];
           else {
             int up = grid[i][j];
-            if(i > 0) up += dp[i-1][j];
+            if(i > 0) up += prev[j];
             else up += 1e9;
             int left = grid[i][j];
-            if(j > 0) left += dp[i][j-1];
+            if(j > 0) left += curr[j-1];
             else left += 1e9;
             
-            dp[i][j] = min(up, left);
+            curr[j] = min(up, left);
           }
         }
+        prev = curr;
       }
-      return dp[n-1][m-1];
+      return prev[m-1];
     }
 };
