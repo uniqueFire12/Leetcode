588efb16d7fcae37108fb325484f5bf7df7895e9
Ananya Ganguly <107770579+uniqueFire12@users.noreply.github.com>
2023-01-16 13:54:23 +0530

    Time: 8 ms (64.11%), Space: 9 MB (53.66%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+  // memoization solution
     int f(int i, int j, vector<vector<int>>& t, int n, vector<vector<int>> &dp) {
       if(i == n-1) {
         return t[n-1][j];
@@ -15,7 +16,20 @@ public:
   
     int minimumTotal(vector<vector<int>>& triangle) {
       int n = triangle.size();
-      vector<vector<int>> dp(n, vector<int>(n, -1));
-      return f(0, 0, triangle, n, dp);
+      vector<vector<int>> dp(n, vector<int>(n, 0));
+      // return f(0, 0, triangle, n, dp);
+      
+      for(int i=0; i<n; i++) {
+        dp[n-1][i] = triangle[n-1][i];
+      }
+      
+      for(int i = n-2; i >= 0; i--) {
+        for(int j = i; j >= 0; j--) {
+          int down = triangle[i][j] + dp[i+1][j];
+          int diag = triangle[i][j] + dp[i+1][j+1];
+          dp[i][j] = min(down, diag);
+        }
+      }
+      return dp[0][0];
     }
 };
