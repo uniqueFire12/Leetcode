fcc3fb25fdbbd15b1b4ca887bad96576a3c36317
Ananya Ganguly <107770579+uniqueFire12@users.noreply.github.com>
2023-07-22 18:31:06 +0530

    Time: 484 ms (7.24%), Space: 98.6 MB (23.23%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
-        unordered_map<int,int> mpp;
-        long long int sum = 0, maxm = 0;
-        for(int i=0; i<nums.size(); i++) {
-            if(mpp.count(nums[i]) == 0) {
-                sum = sum + nums[i];
+        map<int, int> hash;
+        long long int sum = 0, mx = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            if(hash.count(nums[i]) == 0) {
+                sum += nums[i];
             }
-            mpp[nums[i]]++;
-            
-            if(mpp.size() == k) {
-                maxm = max(maxm, sum);
+            hash[nums[i]]++;
+            if(hash.size() == k) {
+                mx = max(mx, sum);
             }
-            
             if(i-k+1 >= 0) {
-                mpp[nums[i-k+1]]--;
-                if(mpp[nums[i-k+1]] == 0) {
+                hash[nums[i-k+1]]--;
+                if(hash[nums[i-k+1]] == 0) {
                     sum = sum - nums[i-k+1];
-                    mpp.erase(nums[i-k+1]);
+                    hash.erase(nums[i-k+1]);
                 }
             }
         }
-        return maxm;
+        return mx;
     }
 };
