d635f988eb8c259e6e169f3f9d1f21e7ede6697c
Ananya Ganguly <107770579+uniqueFire12@users.noreply.github.com>
2023-07-28 18:37:57 +0530

    Time: 35 ms (99.27%), Space: 18.8 MB (77.59%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    double cal(vector<int>& piles, int hourly) {
+       int n = piles.size();
+       double totalhours = 0;
+        for(int i = 0; i < n; i++) {
+            totalhours += ceil((double)piles[i] / (double)hourly);
+        }
+        return totalhours;
+    }
+    
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low = 1, high = *max_element(piles.begin(), piles.end());
+        while(low <= high) {
+            long long int mid = low + (high - low) / 2;
+            double totalHours = cal(piles, mid);
+            if(totalHours <= h) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+};
