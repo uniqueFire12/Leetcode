0de7a9efaf439333acd18ee534701558dd87d8d7
Ananya Ganguly <107770579+uniqueFire12@users.noreply.github.com>
2023-08-21 22:50:05 +0530

    Time: 27 ms (66.17%), Space: 16.8 MB (22.69%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    void dfs(int node, vector<int> &vis, vector<int> adj[]) {
+    void dfs(int node, vector<int> adj[], vector<int> &vis) {
         vis[node] = 1;
         
         for(auto it : adj[node]) {
             if(!vis[it]) {
-                dfs(it, vis, adj);
+                dfs(it, adj, vis);
             }
         }
     }
-    int findCircleNum(vector<vector<int>>& mat) {
+    
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
         int ans = 0;
-        int n = mat.size();
-        vector<int> vis(n+1, 0);
-        vector<int> adj[n];
+        vector<int> vis(n, 0);
         
+        vector<int> adj[n];
         for(int i=0; i<n; i++) {
             for(int j=0; j<n; j++) {
-                if(mat[i][j] == 1 and i != j) {
+                if(isConnected[i][j] == 1 and i != j) {
                     adj[i].push_back(j);
                     adj[j].push_back(i);
                 }
@@ -27,9 +28,10 @@ public:
         for(int i=0; i<n; i++) {
             if(!vis[i]) {
                 ans++;
-                dfs(i, vis, adj);
+                dfs(i, adj, vis);
             }
         }
+        
         return ans;
     }
 };
