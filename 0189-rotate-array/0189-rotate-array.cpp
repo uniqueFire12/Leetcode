f227211e421e49624aaceca1d165dc75942d73b0
Ananya Ganguly <107770579+uniqueFire12@users.noreply.github.com>
2023-09-09 17:46:25 +0530

    Time: 22 ms (71.96%), Space: 24.9 MB (91.06%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -3,8 +3,8 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k = k % n;
+        reverse(nums.begin(), nums.begin()+(n-k));
+        reverse(nums.begin()+(n-k), nums.end());
         reverse(nums.begin(), nums.end());
-        reverse(nums.begin(), nums.begin()+k);
-        reverse(nums.begin()+k, nums.end());
     }
 };
