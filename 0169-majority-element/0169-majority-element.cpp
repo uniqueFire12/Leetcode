12ff39c5261a0b2390d33b883855bbcae44c8995
Ananya Ganguly <107770579+uniqueFire12@users.noreply.github.com>
2023-09-16 23:28:16 +0530

    Time: 21 ms (18.13%), Space: 20 MB (10.57%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        map<int,int> m;
+        for(auto it: nums) m[it]++;
+
+        for(auto it:m) {
+            if(it.second > n/2) {
+                return it.first;
+            }
+        }
+        return -1;
+    }
+};
