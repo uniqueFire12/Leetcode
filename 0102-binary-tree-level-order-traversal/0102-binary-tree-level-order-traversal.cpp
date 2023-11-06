1e1368665c70fba05db2871ab1eda1b98505ac79
Ananya Ganguly <107770579+uniqueFire12@users.noreply.github.com>
2023-11-06 20:40:39 +0530

    Time: 5 ms (49.91%), Space: 13.9 MB (63.91%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -16,22 +16,22 @@ public:
         if(root == NULL) return ans;
         queue<TreeNode*> q;
         q.push(root);
-        
         while(!q.empty()) {
             int size = q.size();
-            vector<int> level;
-            
+            vector<int> temp;
             for(int i = 0; i < size; i++) {
                 TreeNode* node = q.front();
-                q.pop();
-                
-                if(node->left != NULL) q.push(node->left);
-                if(node->right != NULL) q.push(node->right);
-                level.push_back(node->val);
+                q.pop();                
+                if(node->left) {
+                    q.push(node->left);
+                }
+                if(node->right) {
+                    q.push(node->right);
+                }
+                temp.push_back(node->val);
             }
-            ans.push_back(level);
+            ans.push_back(temp);
         }
-        
         return ans;
     }
 };
