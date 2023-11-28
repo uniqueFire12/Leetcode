f170721ce0099d73a81400eeffca87a26dafe918
Ananya Ganguly <107770579+uniqueFire12@users.noreply.github.com>
2023-11-28 17:33:37 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (95.07%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        double ans = 1;
-        long long a = n;
-        
-        if(a < 0) a = -a;
-        while(a > 0) {
-            if(a%2 == 1) {
-                ans = ans*x;
-                a--;
+        double ans = 1.0;
+        long long nn = n;
+        if(n < 0) nn = -1 * nn;
+        while(nn) {
+            if(nn%2) {
+                ans = ans * x;
+                nn = nn - 1;
             }
             else {
-                x = x*x;
-                a /= 2;
+                x = x * x;
+                nn = nn / 2;
             }
         }
-        
-        if(n < 0) {
-            ans = 1.0/ans;
-        }
-        
+        if(n < 0) ans = (double)1.0 / (double)ans;
         return ans;
     }
 };
