5c6b709de2e4bb4fd81a49d8f878ba9d7ffb4d80
Ananya Ganguly <107770579+uniqueFire12@users.noreply.github.com>
2024-01-30 21:45:52 +0530

    Time: 27 ms (69.58%), Space: 29.9 MB (5.51%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -4,12 +4,12 @@ public:
         int low = 0, high = nums.size()-1;
         
         while(low <= high) {
-            int mid = low + (high-low)/2;
-            
+            int mid = low + (high - low)/2;
             if(nums[mid] == target) return mid;
-            else if(target > nums[mid]) low = mid + 1;
+            else if(nums[mid] < target) low = mid + 1;
             else high = mid - 1;
         }
+        
         return -1;
     }
 };
