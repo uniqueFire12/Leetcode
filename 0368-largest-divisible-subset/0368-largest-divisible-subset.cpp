d6a6065b1f37ea5e3ac212c648b51dd8ad44bce4
Ananya Ganguly <107770579+uniqueFire12@users.noreply.github.com>
2024-02-10 01:46:29 +0530

    Time: 16 ms (78.03%), Space: 11.3 MB (41.61%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
-    unordered_map<int, vector<int>> dp;
     vector<int> largestDivisibleSubset(vector<int>& nums) {
-        sort(begin(nums), end(nums));
+        int n = nums.size(), maxi = 1, num = -1;        
+        vector<int> dp(n, 1);
+        sort(nums.begin(), nums.end());
+        
+        for(int i = 1; i < n; i++) {
+           for(int j = 0; j < i; j++) {
+              if((nums[i]%nums[j] == 0) && dp[i]<dp[j]+1) {
+                  dp[i]=dp[j]+1;
+                  
+                  if(maxi < dp[i]) maxi = dp[i];
+              }
+           } 
+        }
+        
         vector<int> ans;
-        for(int i = 0; i < size(nums); i++) {
-            auto res = solve(nums, i);
-            if(size(res) > size(ans)) ans = res;
+        for(int i = n-1; i >= 0; i--) {
+            if(maxi == dp[i] && (num == -1 || !(num%nums[i]))) {
+                ans.push_back(nums[i]);
+                maxi--;
+                num=nums[i];
+            }
         }
+        
         return ans;
-    }
-    vector<int> solve(vector<int>& nums, int start) {
-        if(start >= size(nums)) return {};
-        if(dp.count(start)) return dp[start];
-        for(int next = start+1; next < size(nums); next++) {
-            if(nums[next] % nums[start]) continue;
-            auto res = solve(nums, next);
-            if(size(res) >= size(dp[start])) dp[start] = res;
-        }
-        dp[start].push_back(nums[start]);
-        return dp[start];
     }
 };
