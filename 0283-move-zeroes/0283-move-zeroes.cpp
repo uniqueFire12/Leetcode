6600610cad426906f613daa1e88afb920d9107f3
Ananya Ganguly <107770579+uniqueFire12@users.noreply.github.com>
2024-02-19 01:30:04 +0530

    Time: 16 ms (66.07%), Space: 22.5 MB (5.84%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int zero = 0;
+
         vector<int> temp;
         for(int i=0; i<nums.size(); i++) {
-            if(nums[i] == 0) {
-                zero++;
-            }
-            else if(nums[i] != 0) {
+           if(nums[i] != 0) {
                 temp.push_back(nums[i]);
             }
         }
-        
-        if(zero == 0) return;
         
         int j = 0;        
         for(int i = 0; i < temp.size(); i++) {
@@ -20,9 +15,8 @@ public:
             j++;
         }
         
-        for(int i = 0; i < zero; i++) {
-            nums[j] = 0;
-            j++;
+        for(int i = temp.size(); i < nums.size(); i++) {
+            nums[i] = 0;
         }
     }
 };
