fa5b411ae77c7c54825df608b7218e93de2afd97
Ananya Ganguly <107770579+uniqueFire12@users.noreply.github.com>
2024-04-29 20:22:39 +0530

    Time: 0 ms (100.00%), Space: 9.8 MB (72.06%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
-    void sortColors(vector<int>& arr) {
-        int n = arr.size();
-        int cnt0 = 0, cnt1 = 0, cnt2 = 0;
-        for(int i = 0; i < n; i++) {
-            if(arr[i] == 0) cnt0++;
-            else if(arr[i] == 1) cnt1++;
-            else cnt2++;
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int low = 0, mid = 0, high = n-1;
+        
+        while(mid <= high) {
+            if(nums[mid] == 0) {
+                swap(nums[low++], nums[mid++]);
+            } else if(nums[mid] == 1) {
+                mid++;
+            } else {
+                swap(nums[mid], nums[high--]);
+            }
         }
-        for(int i = 0; i < cnt0; i++) arr[i] = 0;
-        for(int i = cnt0; i < cnt0+cnt1; i++) arr[i] = 1;
-        for(int i = cnt0+cnt1; i < n; i++) arr[i] = 2;
     }
 };
